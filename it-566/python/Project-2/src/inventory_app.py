9bbc08d2773cb8c349912aeb66df3e62ed777b66
Gaston Shefa <gts68564@marymount.edu>
2022-12-05 20:41:26 -0500

    added search function for project-2

--- it-566/python/Project-2/src/inventory_app.py
+++ it-566/python/Project-2/src/inventory_app.py
@@ -17,7 +17,9 @@ class InventoryApp:
 		self.SELECT_INVENTORY = '3'
 		self.LIST_INVENTORY_ITEMS = '4'
 		self.ADD_ITEMS = '5'
-		self.EXIT = '6'
+		self.SEARCH_ITEMS = '6'
+		self.EXIT = '7'
+
 		# Fields
 		self.menu_choice = 1
 		self.keep_going = True
@@ -37,7 +39,8 @@ class InventoryApp:
 		print('\t\t3. Select Inventory')
 		print('\t\t4. List Inventory Items')
 		print('\t\t5. Add Items')
-		print('\t\t6. Exit')
+		print('\t\t6. Search_Items')
+		print('\t\t7. Exit')
 		print()
 
 	def process_menu_choice(self):
@@ -56,9 +59,12 @@ class InventoryApp:
 				self.list_inventory_items()
 			case self.ADD_ITEMS:
 				self.add_items()
+			case self.SEARCH_ITEMS:
+				self.search_items()
 			case self.EXIT:
+
 				if __debug__:
-					print('Goodbye!')
+					print('It was Great Working With You!')
 				self.keep_going = False
 				self.clear_screen()
 			case _:
@@ -79,7 +85,7 @@ class InventoryApp:
 		if __debug__:
 			print('list_inventories() method called...')
 		self.print_inventory_list(self._get_inventories())
-		input('\n\nPress any key to continue...')
+		input('\n\nPress entre to continue...')
 
 	def _get_inventories(self):
 		"""Utility method that calls business logic layer to get all inventory records."""
@@ -111,7 +117,7 @@ class InventoryApp:
 			print('list_inventory_items() method called...')
 		items_list = self.business_logic.get_items_for_inventory_id(self.active_inventory_id)
 		self.print_items_list(items_list)
-		input('\n\nPress any key to continue...')
+		input('\n\nPress entre to continue...')
 
 	def add_items(self):
 		"""Add items to inventory."""
@@ -121,6 +127,27 @@ class InventoryApp:
 		item = input("Enter the name of the item: ")
 		count = int(input(f"Enter the count of {item}:"))
 		self.business_logic.add_new_items(inventory_id=inv_id, item=item, count=count)
+
+	def search_items(self):
+		"""search the item in items table."""
+
+		self.clear_screen()
+		if __debug__:
+			print('Search_items method called...')
+		print('\nHow you want to lookup:\n\t1. Item id \n\t2.Item_name')
+		choice = int(input('\nEnter 1 or 2 :'))
+		if choice == 1:
+			item_id = int(input('Enter the item id:'))
+			item = self.business_logic.search_items_by_id(item_id=item_id)
+			self.print_search_item(item)
+			input('Enter any key to continue...')
+		elif choice == 2:
+			item_name = str(input('Enter the name of the item:'))
+			item = self.business_logic.search_items_by_name(item_name=item_name)
+			self.print_search_item(item)
+			input('Enter any key to continue...')
+		else:
+			print('Enter either 1 or 2.')
 
 	def start_application(self):
 		"""Start the applications."""
@@ -138,5 +165,11 @@ class InventoryApp:
 	def print_items_list(self, items_list):
 		t = PrettyTable(['ID', 'Inventory ID', 'Item', 'Count'])
 		for row in items_list:
+			t.add_row([row[0], row[1], row[2], row[3]])
+		print(t)
+
+	def print_search_item(self, item):
+		t = PrettyTable(['ID', 'Items', 'Count', 'Inventory ID'])
+		for row in item:
 			t.add_row([row[0], row[1], row[2], row[3]])
 		print(t)
